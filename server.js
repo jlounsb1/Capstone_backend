ba7c8919b09d0cd0b5070051b86d654f0cc0e265
jlounsb1 <jefflounsbury@gmail.com>
2024-08-14 16:53:19 -0400

    arg

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import User from './models/users.mjs'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import Dogs from './routes/Dogs.mjs'
-import PlayDates from './routes/Playdates.js'
+import PlayDates from './routes/PlayDates.js'
 
 //app information
 const PORT = 3000;
