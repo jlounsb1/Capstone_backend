4d6de12619acfb2ba46e5a7412df66b10fc6eb67
jlounsb1 <jefflounsbury@gmail.com>
2024-08-14 16:43:08 -0400

    changed file extension to just js

--- routes/PlayDates.mjs
+++ routes/PlayDates.mjs
@@ -1,5 +1,5 @@
 import express from 'express'
-import PlayDate from '../models/Playdates.mjs'
+import PlayDate from '../models/Playdates.js'
 //could not be found when I uploaded to render, trying to change path
 const router = express.Router()
 
