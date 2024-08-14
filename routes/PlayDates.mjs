88ef2cee504701664a8580d710dc34077e830647
jlounsb1 <jefflounsbury@gmail.com>
2024-08-14 16:34:52 -0400

    still trying to find import

--- routes/PlayDates.mjs
+++ routes/PlayDates.mjs
@@ -1,18 +1,18 @@
 import express from 'express'
-import PlayDates from '/models/Playdates.mjs'
+import PlayDate from '../models/Playdates.mjs'
 //could not be found when I uploaded to render, trying to change path
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-    let PlayList = await PlayDates.find({});
+    let PlayList = await PlayDate.find({});
         res.json(PlayList)
     
 })
 
 router.post('/createPlayDate/:id', (req, res) => {
-    console.log(PlayDates, req.body)
-    PlayDates.create(req.body)
-    .then(PlayDates=>res.json(PlayDates))
+    console.log(PlayDate, req.body)
+    PlayDate.create(req.body)
+    .then(PlayDate=>res.json(PlayDate))
     .catch(err => res.json(err))
 })
 
