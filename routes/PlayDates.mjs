910308784a6d6969789db928f126e9339f5dd458
jlounsb1 <jefflounsbury@gmail.com>
2024-08-14 16:24:51 -0400

    having trouble with routing in render, adjusting according to log

--- routes/PlayDates.mjs
+++ routes/PlayDates.mjs
@@ -1,5 +1,6 @@
 import express from 'express'
-import PlayDates from '../models/Playdates.mjs'
+import PlayDates from './models/Playdates.mjs'
+//could not be found when I uploaded to render, trying to change path
 const router = express.Router()
 
 router.get('/', async (req, res) => {
